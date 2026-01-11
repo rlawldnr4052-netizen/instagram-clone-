eba7b7872ce1ca6fd0d7aa323be604fd5e9ca4a3
kimjiwook123 <rlawldnr4052@naver.com>
2026-01-11 22:39:51 +0900

    Deploy: Fresh static build for Vercel

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d8626bf07b462982bbfb3b517a8b6996",
+const RESOURCES = {"flutter_bootstrap.js": "ca472cf1c57bf99b5be802b37db7e123",
 ".vercel/project.json": "98bce28e37a1823de462b9f68c00da53",
 ".vercel/README.txt": "2b13c79d37d6ed82a3255b83b6815034",
 "version.json": "95fbfc7541202ec2ba475fbb88a9a2a3",
